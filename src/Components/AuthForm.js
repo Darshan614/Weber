d582e2342d106753ee09cde1ea6cff3bdcb96d81
Darshan614 <jaindarshan614@gmail.com>
2022-03-26 11:43:42 +0530

    AuthPage developed

--- src/Components/AuthForm.js
+++ src/Components/AuthForm.js
@@ -1,56 +1,73 @@
 import React from 'react';
 import classes from './AuthForm.module.css';
-import { styled } from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
-// import CssBaseline from '@mui/material/CssBaseline';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
-import Icon from '@mui/material/Icon';
-import MdPhone from '@mui/icons-material/Phone';
-import Chip from '@mui/material/Chip';
-
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-}));
 
 function AuthForm() {
-  return (
+  const FormHeader = (props) => (
+    <h2 className={classes.headerTitle}>{props.title}</h2>
+  );
+
+  const Form = (props) => (
     <div>
-      <Container className={classes.container} maxWidth="xs">
-        <h2 className={classes.title}>LOGIN</h2>
-        <Box>
-          <Grid container spacing={2}>
-            <Grid item xs={4} md={8}>
-              <Item>Email</Item>
-            </Grid>
-            <Grid item xs={8} md={4}>
-              <input type="email" className={classes.input} />
-            </Grid>
-            <Grid item xs={4} md={4}>
-              <Item>Password</Item>
-            </Grid>
-            <Grid item xs={8} md={8}>
-              <input type="password" className={classes.input} />
-            </Grid>
-            <Grid item xs={7.5} md={7.5}>
-              <Button
-                variant="contained"
-                color="success"
-                className={classes.button}
-                size="small"
-                end={MdPhone}
-              >
-                Login
-              </Button>
-            </Grid>
-          </Grid>
-        </Box>
-      </Container>
+      <FormInput
+        description="Username"
+        placeholder="Enter your username"
+        type="text"
+      />
+      <FormInput
+        description="Password"
+        placeholder="Enter your password"
+        type="password"
+      />
+      <FormButton title="Log in" />
+    </div>
+  );
+
+  const FormButton = (props) => (
+    <div className={classes.button} className={classes.row}>
+      <button>{props.title}</button>
+    </div>
+  );
+
+  const FormInput = (props) => (
+    <div className={classes.row}>
+      <label>{props.description}</label>
+      <input type={props.type} placeholder={props.placeholder} />
+    </div>
+  );
+
+  const OtherMethods = (props) => (
+    <div className={classes.alternativeLogin}>
+      <label>Or sign in with:</label>
+      <div className={classes.iconGroup}>
+        <Facebook />
+        <Twitter />
+        <Google />
+      </div>
+    </div>
+  );
+
+  const Facebook = (props) => (
+    <a href="#" className={classes.facebookIcon}>
+      f
+    </a>
+  );
+
+  const Twitter = (props) => (
+    <a href="#" className={classes.twitterIcon}>
+      t
+    </a>
+  );
+
+  const Google = (props) => (
+    <a href="#" className={classes.googleIcon}>
+      G
+    </a>
+  );
+  return (
+    <div className={classes.loginform}>
+      <FormHeader title="Login" />
+      <Form />
+      <OtherMethods />
     </div>
   );
 }
